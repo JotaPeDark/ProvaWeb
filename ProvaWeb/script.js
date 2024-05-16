4a11eeb4d89d4b23a782d8f04feb3719c983d7e9
JotaPeDark <127147823+JotaPeDark@users.noreply.github.com>
2024-05-16 18:44:09 -0300

    1605

--- ProvaWeb/script.js
+++ ProvaWeb/script.js
@@ -1,11 +1,19 @@
-const iconeFiltro = document.getElementById('iconeFiltro');
-const modal = document.getElementById('modal');
-const fecharModal = document.getElementById('fecharModal');
+document.addEventListener('DOMContentLoaded', function () {
+    const iconeFiltro = document.getElementById('iconeFiltro');
+    const modal = document.getElementById('modal');
+    const fecharModal = document.getElementById('fecharModal');
+    const aplicarFechar = document.getElementById('aplicarFechar');
 
-iconeFiltro.addEventListener('click', function() {
-    modal.showModal();
+    iconeFiltro.addEventListener('click', function () {
+        modal.showModal(); 
+    });
+
+    fecharModal.addEventListener('click', function () {
+        modal.close();
+    });
+
+    aplicarFechar.addEventListener('click', function (event) {
+        event.preventDefault();
+        modal.close();
+    });
 });
-
-fecharModal.addEventListener('click', function() {
-    modal.close();
-})
